fa6e1a76529e2cc4984f4c3a68d93d05cb21f9ef
ApsharaTasnim <alferdstanley@gmail.com>
2024-10-18 21:22:47 +0600

    adddatatypes

---
+++ main.py
@@ -0,0 +1,21 @@
+name="Apshara"
+age=17
+is_student=True
+weight=48.5
+
+print("Name:",name)
+print("Data Type of name is",type(name))
+print("Age:",age)
+print("Data Type of name is",type(age))
+print("is_student:",is_student)
+print("Data Type of name is",type(is_student))
+print("Weight:",weight)
+print("Data Type of name is",type(weight))
+
+print("\n After Type Casting.....")
+age=str(age)
+print(age)
+print("Data Type of name is",type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of name is",type(weight))
